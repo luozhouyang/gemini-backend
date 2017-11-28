dd2400c257ff157632472623135f72f126a28235
luozhouyang <stupidme.me.lzy@gmail.com>
2017-11-28 21:24:56 +0800

    Create login controller

--- controllers/login.go
+++ controllers/login.go
@@ -1,1 +1,20 @@
 package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+)
+
+type LoginController struct {
+	beego.Controller
+}
+
+var log = logs.GetLogger("gemini-backend")
+
+func (c *LoginController) Get() {
+	log.Print("Login Get method called.")
+}
+
+func (c *LoginController) Post() {
+	log.Print(c.Ctx.Request.PostForm)
+}
