d0787d6cc8c859d483103416c8acb5062b1ea6cc
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-19 22:07:31 +0800

    Create article model

--- models/article.go
+++ models/article.go
@@ -1,1 +1,48 @@
 package models
+
+import (
+	"time"
+	"github.com/astaxie/beego/orm"
+)
+
+func init() {
+	orm.RegisterModel(new(Article))
+	orm.RunSyncdb(mysqldb, false, true)
+}
+
+type Article struct {
+	Id       int64     `orm:"auto"`
+	Title    string    `orm:"size(20)"`
+	Author   Author    `orm:"rel(fk)"`
+	Time     time.Time
+	Content  string
+	Comments []Comment `orm:"reverse(many)"`
+}
+
+func InsertArticle(a Article) (err error) {
+	o := orm.NewOrm()
+	//o.Using(mysqldb)
+	_, err = o.Insert(a)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteArticleById(id int64) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Delete(&Article{Id: id})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteArticle(a *Article) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Delete(&a)
+	if err != nil {
+		return err
+	}
+	return nil
+}
