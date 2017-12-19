b128f1e11926a541a37ff6ae3a6a7e9c55b9f250
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-19 22:31:49 +0800

    Create author model

--- models/author.go
+++ models/author.go
@@ -1,1 +1,44 @@
 package models
+
+import "github.com/astaxie/beego/orm"
+
+func init() {
+	orm.RegisterModel(new(Author))
+	orm.RunSyncdb(mysqldb, false, true)
+}
+
+type Author struct {
+	Id      int64  `orm:"auto"`
+	Name    string `orm:"size(20)"`
+	Bio     string
+	Website string
+	Email   string
+	Github  string
+}
+
+func InsertAuthor(a *Author) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Insert(&a)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteAuthor(a *Author) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Delete(&a)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteAuthorById(id int64) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Delete(&Author{Id: id})
+	if err != nil {
+		return err
+	}
+	return nil
+}
