e807f1a523a4b2c42a4a76185c0fcc82c8655a4d
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-19 22:25:03 +0800

    Create comment model

--- models/comment.go
+++ models/comment.go
@@ -1,1 +1,41 @@
 package models
+
+import "github.com/astaxie/beego/orm"
+
+func init() {
+	orm.RegisterModel(new(Comment))
+	orm.RunSyncdb(mysqldb, false, true)
+}
+
+type Comment struct {
+	Id      int64  `orm:"auto"`
+	Author  Author `orm:"reverse(one)"`
+	Content string
+}
+
+func InsertComment(c *Comment) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Insert(&c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteComment(c *Comment) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Delete(&c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteCommentById(id int64) (err error) {
+	o := orm.NewOrm()
+	_, err = o.Delete(&Comment{Id: id})
+	if err != nil {
+		return err
+	}
+	return nil
+}
