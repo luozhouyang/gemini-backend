fad0148774c33d799b8b73f584b020dd4d07d783
luozhouyang <stupidme.me.lzy@gmail.com>
2017-12-21 22:51:35 +0800

    Fixed #2 by adding default config values

--- models/orm_mysql.go
+++ models/orm_mysql.go
@@ -7,13 +7,13 @@ import (
 )
 
 func init() {
-	mysqluser := beego.AppConfig.String("mysqluser")
-	mysqlpass := beego.AppConfig.String("mysqlpass")
-	mysqlurl := beego.AppConfig.String("mysqlurls")
-	mysqldbname := beego.AppConfig.String("mysqldb")
+	mysqluser := beego.AppConfig.DefaultString("mysqluser", "gemini")
+	mysqlpass := beego.AppConfig.DefaultString("mysqlpass", "usergemini")
+	mysqlurl := beego.AppConfig.DefaultString("mysqlurls", "localhost:3306")
+	mysqldbname := beego.AppConfig.DefaultString("mysqldb", "gemini_db")
 	datasource := mysqluser + ":" + mysqlpass + "@tcp(" + mysqlurl + ")/" + mysqldbname + "?charset=utf8"
 	orm.RegisterDriver("mysql", orm.DRMySQL)
-	datasource = "gemini:usergemini@tcp(localhost:3306)/gemini_db?charset=utf8"
+	//datasource = "gemini:usergemini@tcp(localhost:3306)/gemini_db?charset=utf8"
 	orm.RegisterDataBase("default", "mysql", datasource)
 	orm.RegisterModel(new(Article), new(User), new(Comment))
 	orm.RunSyncdb("default", false, true)
