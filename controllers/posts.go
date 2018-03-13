00ddcb404c7db73868def338fc1bb6387b1aea5e
luozhouyang <stupidme.me.lzy@gmail.com>
2018-03-13 21:50:35 +0800

    Create posts controller

--- controllers/posts.go
+++ controllers/posts.go
@@ -1,1 +1,15 @@
 package controllers
+
+import "github.com/astaxie/beego"
+
+type PostsController struct {
+	beego.Controller
+}
+
+func (c *PostsController) Get(){
+
+}
+
+func (c *PostsController) Post(){
+
+}
