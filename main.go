0c358d7396bc958dc544646f51096132f43bfab4
luozhouyang <stupidme.me.lzy@gmail.com>
2018-03-13 21:45:39 +0800

    Update article model

--- main.go
+++ main.go
@@ -3,9 +3,21 @@ package main
 import (
 	_ "backend/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
 
-func main() {
-	beego.Run()
+func init() {
+	beego.BConfig.WebConfig.AutoRender = false
+	usr := beego.AppConfig.String("dev::mysqluser")
+	pass := beego.AppConfig.String("dev::mysqlpass")
+	url := beego.AppConfig.String("dev::mysqlurls")
+	db := beego.AppConfig.String("dev::mysqldb")
+	ds := usr + ":" + pass + "@tcp(" + url + ")/" + db + "?charset=utf-8"
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", ds, 1, 10)
 }
 
+func main() {
+	orm.Debug = true
+	beego.Run()
+}
--- models/article.go
+++ models/article.go
@@ -3,16 +3,20 @@ package models
 import (
 	"time"
 	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+	orm.RegisterModel(new(Article))
+}
+
 type Article struct {
-	Id       int64      `orm:"auto"`
-	Title    string     `orm:"size(20)"`
-	Author   *User      `orm:"rel(fk)"`
-	Created  time.Time  `orm:"auto_now_add"`
-	Updated  time.Time  `orm:"auto_now"`
-	Content  string     `orm:"size(65535)"`
-	Comments []*Comment `orm:"reverse(many)"`
+	Id      int64     `orm:"auto"`
+	Title   string    `orm:"size(20)"`
+	Author  string    `orm:"size(20)"`
+	Created time.Time `orm:"auto_now_add"`
+	Updated time.Time `orm:"auto_now"`
+	Content string    `orm:"size(65535)"`
 }
 
 func InsertArticle(a *Article) error {
@@ -40,4 +44,36 @@ func DeleteArticle(a *Article) error {
 		return err
 	}
 	return nil
+}
+
+func UpdateArticle(a *Article) error {
+	o := orm.NewOrm()
+	_, err := o.Update(a)
+	if err != nil {
+		return err
+	} else {
+		return nil
+	}
+}
+
+func QueryArticleById(id int64) *Article {
+	o := orm.NewOrm()
+	a := Article{Id: id}
+	err := o.Read(&a, "")
+	if err != nil {
+		return nil
+	} else {
+		return &a
+	}
+}
+
+func QueryArticleByTitle(title string) error {
+	o := orm.NewOrm()
+	a := Article{Title: title}
+	err := o.Read(&a, "")
+	if err != nil {
+		return nil
+	} else {
+		return &a
+	}
 }
