8e818f52e6300050a3ff4b7fd7e8e802ea01e15a
luozhouyang <stupidme.me.lzy@gmail.com>
2018-03-13 21:46:31 +0800

    Rmeove useless codes

--- routers/router.go
+++ routers/router.go
@@ -6,6 +6,5 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
-    beego.Router("/login",&controllers.LoginController{})
+	beego.Router("/", &controllers.MainController{})
 }
