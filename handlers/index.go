754dde535d57b7a65b9c5ebeca29c89c9f9a9fc2
luozhouyang <stupidme.me.lzy@gmail.com>
2018-05-01 13:20:06 +0800

    Create index handler

--- handlers/index.go
+++ handlers/index.go
@@ -1,1 +1,10 @@
 package handlers
+
+import "net/http"
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+}
