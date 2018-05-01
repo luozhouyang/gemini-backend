ed0e14e0924f8f3a71d2a681d93d96588352914e
luozhouyang <stupidme.me.lzy@gmail.com>
2018-05-01 13:57:36 +0800

    Refactor postgresql ops

--- db/postgres.go
+++ db/postgres.go
@@ -2,37 +2,74 @@ package db
 
 import (
 	"database/sql"
-	"io/ioutil"
-	"encoding/json"
 	"log"
 	_ "github.com/lib/pq"
+	"fmt"
 )
 
-var Db *sql.DB
+var ds string
 
 func init() {
-	f, err := ioutil.ReadFile("db/db.json")
-	if err != nil {
-		log.Fatal(err)
+	config := &config{
+		Host:     "0.0.0.0",
+		Port:     "8080",
+		User:     "gemini",
+		Password: "usergemini",
+		DbName:   "geminidb",
+		SSLMode:  "disable",
 	}
-	config := &config{}
-	err = json.Unmarshal(f, config)
+	ds := config.DataSource()
+	println(ds)
+	err := createDatabase()
 	if err != nil {
-		log.Fatal(err)
-	}
-	ds := "host=" + config.Host + " port=" + config.Port + " user=" + config.Port + " password=" + config.Password +
-		" dbname=" + config.DbName + " sslmode=" + config.SSLMode
-	Db, err = sql.Open("postgres", ds)
-	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
 
+func NewDataBase() (*sql.DB, error) {
+	db, err := sql.Open("postgres", ds)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return db, nil
+}
+
+const CreateTable = "CREATE TABLE IF NOT EXISTS articles (" +
+	"id SERIAL PRIMARY KEY, " +
+	"uuid VARCHAR(64) UNIQUE NOT NULL, " +
+	"tags VARCHAR(255), " +
+	"title VARCHAR(255) NOT NULL, " +
+	"author VARCHAR(255) NOT NULL, " +
+	"content TEXT NOT NULL, " +
+	"updated_at TIMESTAMP NOT NULL, " +
+	"created_at TIMESTAMP NOT NULL" +
+	")"
+
+func createDatabase() error {
+	db, err := NewDataBase()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	_, err = db.Exec(CreateTable)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 type config struct {
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	User     string `json:"user"`
-	Password string `json:"password"`
-	DbName   string `json:"dbname"`
-	SSLMode  string `json:"sslmode"`
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DbName   string
+	SSLMode  string
+}
+
+func (c *config) DataSource() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Port, c.DbName, c.SSLMode)
 }
